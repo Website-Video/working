d9c66cf9d93889cad702c308de2fad98c6bd1bb3
Andy Knudsen <andy@websitetalkingheads.com>
2018-06-01 01:38:44 -0600

    working

---
+++ docs/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Website Video</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<h1>Test</h1>
+	<p>This is a test</p>
+</body>
+</html>
